2c352760e2b26e278805ca99d9a2f6c53013c4f0
praty25 <prateek@peoplelinkvc.com>
2016-04-29 13:08:41 +0530

    added files

---
+++ imports/signuppage/signup.js
@@ -0,0 +1,27 @@
+import { Template } from 'meteor/templating';
+import { Accounts } from 'meteor/accounts-base'
+import './signup.html';
+
+
+Template.signup.events({
+	'submit #form2'(event){
+		event.preventDefault();
+		var options = {
+			email : $("#email").val(),
+			password : $("#password").val(),
+			profile:{
+			name : $("#name").val(),
+			date : $("#dob").val(),
+			gender : $("#gender:checked","#form2").val()
+			}
+		};
+		Accounts.createUser(options,function(error){
+			if(error){
+				console.log("error:",error.reason)
+			}
+			else{
+				Router.go('/')
+			}
+		});
+	}
+})
